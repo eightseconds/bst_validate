d4a35946477fe16c1ef3c1fdad12f94e51596a0b
eightseconds <paulseokcho@gmail.com>
2019-10-10 17:14:04 -0700

    Implement naive solution

--- validateTree.js
+++ validateTree.js
@@ -1,3 +1,9 @@
 function validateBst(tree) {
-    
+    const minValue = -Infinity
+    const maxValue = Infinity
+
+    if (tree === null) return true;
+    if (tree.value < minValue || tree.value >= maxValue) return false;
+    const leftValid = validateBst(tree.left)
+    return leftValid && validateBst(tree.right)
 }
