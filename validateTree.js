63755b4198880e280e7db31329a1497c6f5fb8c2
eightseconds <paulseokcho@gmail.com>
2019-10-11 18:34:39 -0700

    Update function

--- validateTree.js
+++ validateTree.js
@@ -5,6 +5,7 @@ function validateBst(tree) {
 function validateBstHelper(tree, minValue, maxValue) {
     if (tree === null) return true;
     if (tree.value < minValue || tree.value >= maxValue) return false;
+    
     const leftValid = validateBstHelper(tree.left, minValue, tree.value)
     return leftValid && validateBstHelper(tree.right, tree.value, maxValue)
 }
